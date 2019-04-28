66dcf259dcbca11ce79593b1add0e4560342aec4
Tobias Trinks <tobias.trinks99@gmx.net>
2019-04-28 16:23:23 +0200

    Change hello world message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello Split Testing!');
+    res.send('Hello Split Testing!!!');
 });
 
 app.listen(3000, function () {
